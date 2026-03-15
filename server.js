94a1d783b097c11914433558a7c15f8c4a91e792
MLisa95 <lgasa@byupathway.edu>
2026-03-15 16:36:45 +0200

    site update, render working

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 
+//  index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -38,7 +43,3 @@ app.listen(port, () => {
 })
 
 
-//  index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
